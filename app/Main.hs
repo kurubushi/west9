09927569f291002cbd72798b05c8e3d6c8c798a8
kurubushi <krbshi@gmail.com>
2016-02-25 12:13:10 +0900

    change: cmdargs into getOpt
    でも，だめー．

--- app/Main.hs
+++ app/Main.hs
@@ -2,36 +2,47 @@
 
 module Main where
 
-import West9 (tweetNow, tweetRep, ig, makeOAuthEnv, defaultOAuth)
+import West9 (tweetNow, tweetRep, ig, makeOAuthEnv)
 import System.IO (putStrLn,hFlush,stdout,appendFile)
 import System.Environment (getArgs)
 import System.Directory (getHomeDirectory)
-import System.Console.CmdArgs (
-  (&=), explicit, name, argPos, cmdArgs, help, Data, Typeable)
+import System.Console.GetOpt
 import Control.Monad (liftM)
 import Control.Exception (IOException,handle,displayException,SomeException)
 import Control.Monad.Reader (runReaderT)
+import Data.List (foldl')
+import Data.Maybe (fromMaybe)
 
 data Options = Options {
   optRepID :: Maybe Integer
-, optTweetText :: String
-} deriving (Show, Eq, Data, Typeable)
+, optConfigFilePath :: FilePath
+} deriving (Show, Eq)
 
-defaultOption :: Options
-defaultOption = Options {
-  optRepID = Nothing &= explicit
-    &= name "reply" &= name "r"
-    &= help "which tweetID in reply to"
-, optTweetText = "" &= argPos 0
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['r'] ["reply"]
+      (OptArg (\mst opts -> opts {optRepID=liftM read mst}) "TweetID")
+      "which TweetID in reply to"
+  , Option [] ["oauth"]
+      (OptArg ( (\fp opts -> opts {optConfigFilePath=fp}) . fromMaybe "$HOME/.west9/oauth.conf")
+        "FILE")
+      "OAuth config file path"
+  ]
+
+defaultOpts :: Options
+defaultOpts = Options {
+  optRepID = Nothing
+, optConfigFilePath = "$HOME/.west9/oauth.conf"
 }
 
 main ::  IO ()
 main = handle ig $ do
-  homeDir <- getHomeDirectory
-  oauthEnv <- makeOAuthEnv $ homeDir++"/.west9/oauth.config"
-  let oauthEnv = defaultOAuth
-  opts <- cmdArgs defaultOption
-  let tw = optTweetText opts
+  args <- getArgs
+  (opts, tw) <- case getOpt Permute options args of
+    (optf, [rest], []) -> return (foldl' (flip id) defaultOpts optf, rest)
+    (_,_,error       ) -> fail $ show error
+  let oauthFilePath = optConfigFilePath opts
+  oauthEnv <- makeOAuthEnv oauthFilePath
   case optRepID opts of
     (Just id) -> runReaderT (tweetRep tw id) oauthEnv
     Nothing   -> runReaderT (tweetNow tw   ) oauthEnv
